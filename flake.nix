7a3f71026f3e739774b08a0ed4af3d2ffdfab948
Levente Hagymasi <levi@Leventes-MacBook-Air.local>
2026-03-01 19:46:23 +0100

    chore: add flake-utils to nix dev shell

--- flake.nix
+++ flake.nix
@@ -1,28 +1,31 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, ... }:
-    let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-    in {
-        devShells.${system}.default = pkgs.mkShell {
-            packages = with pkgs; [
-              pkg-config
-              openssl
-              protobuf
-              rustup
-              just
-              redis
-            ];
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pkg-config
+            openssl
+            protobuf
+            rustup
+            just
+            redis
+          ];
 
-            env = {
-                PROTOBUF_LOCATION = "${pkgs.protobuf}";
-                PROTOC = "${pkgs.protobuf}/bin/protoc";
-                PROTOC_INCLUDE = "${pkgs.protobuf}/include";
-            };
+          env = {
+            PROTOBUF_LOCATION = "${pkgs.protobuf}";
+            PROTOC = "${pkgs.protobuf}/bin/protoc";
+            PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+          };
         };
-    };
+      }
+    );
 }
